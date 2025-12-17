026471f966fecf86fde9c7d72bea5c6f9414fb06
muhammedjefin216-ship-it <muhammedjefin216@gmail.com>
2025-12-17 16:47:53 +0530

    alert-box

--- lib/login.dart
+++ lib/login.dart
@@ -1,34 +1,22 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
 
-class _LoginPageState extends State<LoginPage> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("LoginPage"),),
-
-     body: Center(
-      
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-          SizedBox(height: 5);
-          
-        },
-        child: Text('Login'),
+      appBar:AppBar(
+        title: Text("Home",
+        style: TextStyle(
+          color: Colors.indigo
+          fontWeight:FontWeight.bold
+        ),
+        
+        ),
       ),
-     ), 
     );
-
   }
 }
